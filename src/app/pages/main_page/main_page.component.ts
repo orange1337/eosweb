17db9808ef17bfd37f38996a8eb954360f71a6cc
orange1337 <orange@MacBook-Air-orange.local>
2018-05-27 13:47:14 +0300

    mainpage

---
+++ src/app/pages/main_page/main_page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-page',
+  templateUrl: './main.page.component.html'
+})
+export class MainPageComponent {}
---
+++ src/app/pages/main_page/main_page.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { MainPageComponent } from './main.page.component';
+import { MainTableModule } from '../../components/main_table/main_table.module';
+import { MatAutocompleteModule,
+         MatButtonModule,
+         MatInputModule,
+         MatPaginatorModule,
+         MatProgressSpinnerModule,
+         MatSelectModule,
+         MatSortModule,
+         MatTableModule,
+         MatFormFieldModule } from '@angular/material';
+//import { MainPageService } from '../../services/main_page.service';
+import { CommonModule } from '@angular/common';
+import { appRoutes } from '../../main.router';
+
+
+let imports = [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule,
+    CommonModule,
+    appRoutes,
+    MainTableModule ];
+
+@NgModule({
+  declarations: [
+    MainPageComponent
+  ],
+  imports:  imports,
+  providers: [ /*MainPageService*/ ],
+  bootstrap: [ MainPageComponent ]
+})
+export class MainPageModule {}
+
+
