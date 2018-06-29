8a8e067cb1021b0e5c163a0a62d9112c9e8adc51
orange1337 <rost@MacBook-Pro-rost.local>
2018-06-29 09:38:59 +0300

    Bp.json

--- server/router/main.router.js
+++ server/router/main.router.js
@@ -1,11 +1,16 @@
 /*
 * Created by Rost
 */
+const path = require('path');
 
 module.exports = function(router, config, request, log) {
 
 	router.get('/', (req, res) => {
 	   	res.sendFile('index.html');
+	});
+
+	router.get('/bp.json', (req, res) => {
+	   	res.sendFile(path.join(__dirname, '../../bp.json'));
 	});
 
 // ============== END of exports 
